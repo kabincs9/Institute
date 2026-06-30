e733830112de17a5ffc7ee3bdf29c6b3b7030cc1
Kabin Baniya <kabinbaniya9@gmail.com>
2026-06-30 21:28:32 +0545

    Lab 6

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
+class Distance {
+    float meter;
+public:
+    Distance(float m) : meter(m) {}
+
+    int convertToFeet() {
+        return static_cast<int>(meter * 3.280);
+    }
+};
+
+int convertMeterToFeet(float meter) {
+    return static_cast<int>(meter * 3.280);
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    float meterValue = 5.0;
+    Distance distanceObj(meterValue);
+    int feet = distanceObj.convertToFeet();
+    std::cout << meterValue << " meter(s) = " << feet << " feet" << std::endl;
     return 0;
 }
